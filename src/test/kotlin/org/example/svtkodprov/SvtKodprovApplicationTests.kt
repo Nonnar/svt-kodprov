c3f290b37309d6b66ce7f0c9f893805a71d65787
Christoffer Skogsberg <christoffer.skogsberg@gmail.com>
2024-12-07 21:22:50 +0100

    Created project

---
+++ src/test/kotlin/org/example/svtkodprov/SvtKodprovApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.svtkodprov
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SvtKodprovApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
